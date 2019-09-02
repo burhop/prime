69739c2cfc6e83ca3cca72a61eee510d8c647e45
burhop <mark.burhop@siemens.com>
2019-09-02 15:11:07 -0400

    Convert code to use Boost dynamic bitset

--- Prime/Prime.h
+++ Prime/Prime.h
@@ -1,39 +1,18 @@
 #pragma once
-/**
-	This class is for calculating and storing prime numbers. It uses the Sieve of Eratosthenes to mark 
-	bits in a block of memory. As there are an infinite number of prime numbers, this class can apply the Sieve
-	to an ever increasing list of blocks. BLock size is set to 4294967280 (0xFFFFFFF0) by default but can be recompiled
-	by defining PRIMEMAX.  Note that PRIMEMAX must be devisible by 2,3, and 5 to keep blocks alligned.
-	
-	For version 0.2, it is limited to size_t (64 bits).  The plan is to extend thsi out to __int128 (128 bits) or use
-	one of the "infinite" bit classes for integers in some futer versio0.
-
-	Storage of the prime numbers, at least the initial numbers, is inefficient as a list.  Storage of a bit for each
-	number (true for prime) is initially more efficient.  This list of bits can be cut in 1/2 by only storing odd numbers.
-	It can be further cut by 1/3 by eliminating numbers divisible by 3, 1/5 by eliminating numbers devisible by 5 and so on.
-	Since future cuts beyond 5 give diminishing returns, version 0.1 stops here. I hypothosize that a list of numbers will
-	eventually be the most effiecient for higher ranges. I invite anyone with a more efficient method let me know of
-	your solution so I can futher compress the data.
-
-	P.S. The most efficient way to store these numbers is to just store the algorithm.  However, we want to be able to
-	find the numbers in roughly constant time.
-
-	https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
-
-	@author Mark Burhop
-	@version 0.1 8/20/2019
-*/
 #include <string>
 #include <vector>
 #include <bitset>
-// The maximum size of a block of memory. Default is max for VS C++ default x64 compiler. 
-#ifndef PRIMEMAX
-#define PRIMEMAX  3000000 // 0xFFFFFFF0 //highest number divisiable by 30
-#endif
+#include <boost/dynamic_bitset.hpp>
 
-#if( PRIMEMAX % 120 != 0 )
-#error "Bitset block size PRIMEMAX must be divisible by 30 (2*3*5) "
-#endif
+//// The maximum size of a block of memory. Default is max for VS C++ default x64 compiler. 
+//#ifndef PRIMEMAX
+//#define PRIMEMAX  3000000 // 0xFFFFFFF0 //highest number divisiable by 30
+//#endif
+
+//#if( PRIMEMAX % 120 != 0 )
+//#error "Bitset block size PRIMEMAX must be divisible by 30 (2*3*5) "
+//#endif
+
 /**
 	This class is for calculating and storing prime numbers. It uses the Sieve of Eratosthenes to mark
 	bits in a block of memory. As there are an infinite number of prime numbers, this class can apply the Sieve
@@ -62,10 +41,14 @@ class __declspec(dllexport) Prime
 {
 
 public:
-	/** Constructor */
-	Prime();
+	/** Constructor. By default block size will be 0xFFFFFFF0 */
+	//Prime();
+	/** Constuctor.  Set the block size explicitly.  This can not be changed later. */
+	Prime(size_t bitsetSize=0xFFFFFFF0);
 	/** Destructor */
 	~Prime();
+	/** returns the size of the bit blocks.  That is, how many bit each block of data stores */
+	size_t GetBitBlockSize();
 	/** Tells the Object to save each chunk of bits as it gets fully set. This is useful if you are searching 
 	    for a large set of prime numbers and want to be sure you don't lose data to a catastrphic failure.
 	@param b If true, saves each set of bits to a file as it is completed.*/
@@ -97,36 +80,36 @@ public:
 		@return The number of prime numbers between and including the two numbers
 		*/
 	size_t CountPrimes(size_t lowVal, size_t highVal);
-	/** the size of the data block the library was compiled with. */
-	static const size_t BitBlockSize = PRIMEMAX;
 
 private:
-	static const size_t max = (size_t)PRIMEMAX;           // 4294967295;
-	static const size_t max2 = max / 2 + max % 2;         // don't need to save the even numbers
-	static const size_t max3 = 2 * max2 / 3 + max2 % 3;   // don't need to save numbers divisible by 3
-	static const size_t max5 = 4 * max3 / 5 + max3 % 5;   // don't need to save numbers divisible by 5
+	/** the size of the data block the library was compiled with. */
+	size_t bitBlockSize=0;
+	size_t max =  0; //= (size_t)PRIMEMAX;           // 4294967295;
+	size_t max2 = 0; // max / 2 + max % 2;         // don't need to save the even numbers
+	size_t max3 = 0; // 2 * max2 / 3 + max2 % 3;   // don't need to save numbers divisible by 3
+	size_t max5 = 0; // 4 * max3 / 5 + max3 % 5;   // don't need to save numbers divisible by 5
 	size_t searchDisttance = 0;							  // Fartherest this class instance has search so far.
 	//std::string baseFileName="primes";
 	bool saveIcrementalFiles = false;
 	//Compiler worries this vector might get passed out of the DLL. Turn this warning off
 #pragma warning( push )   
 #pragma warning( disable : 4251)
-	std::vector<std::bitset<max>*> vectorOfBitSets;       // Place to save our bitsets
+	std::vector<boost::dynamic_bitset<> *> vectorOfBitSets;       // Place to save our bitsets
 #pragma warning(pop)
 
 	//Private Functions
 	void findFirstBlockOFPrimes();
-	size_t NextPrime(std::bitset<Prime::max >* bSet, size_t index);
-	void writeSparseBitSetToStream(const std::string fileName, std::bitset<Prime::max> *bitss);
-	void writeBitSetToStream(const std::string fileName, std::bitset<max5>* bitss);
-	void readSparseBitSetFromStream(const char * my_file, std::bitset<Prime::max>* b);
-	void readBitSetFromStream(const std::string my_file, std::bitset<Prime::max5>* b);
+	size_t NextPrime(boost::dynamic_bitset<>* bSet, size_t index);
+	void writeSparseBitSetToStream(const std::string fileName, boost::dynamic_bitset<> *bitss);
+	void writeBitSetToStream(const std::string fileName, boost::dynamic_bitset<>* bitss);
+	void readSparseBitSetFromStream(const char * my_file, boost::dynamic_bitset<>* b);
+	void readBitSetFromStream(const std::string my_file, boost::dynamic_bitset<>* b);
 	void clearBitsetVector();
-	std::bitset<Prime::max>* primeSieve();
-	size_t countPrimes(std::bitset<Prime::max>* b);
-	std::bitset<Prime::max>* primeSieve2(std::vector<std::bitset<Prime::max>*> vec);
-	void compressBitSet(std::bitset<Prime::max>* iBitSet, std::bitset<Prime::max5>* b);
-	void uncompressBitSet(std::bitset<Prime::max5>* iBitSet, std::bitset<Prime::max>* b);
+	boost::dynamic_bitset<>* primeSieve();
+	size_t countPrimes(boost::dynamic_bitset<>* b);
+	boost::dynamic_bitset<>* primeSieve2(std::vector<boost::dynamic_bitset<>*> vec);
+	void compressBitSet(boost::dynamic_bitset<>* iBitSet, boost::dynamic_bitset<>* b);
+	void uncompressBitSet(boost::dynamic_bitset<>* iBitSet, boost::dynamic_bitset<>* b);
 };
 
 
--- PrimeExamples/PrimeExamples.cpp
+++ PrimeExamples/PrimeExamples.cpp
@@ -11,14 +11,14 @@
 #include <bitset>
 int main(int argc, char*argv[])
 {
-	//Create our Class
-	Prime prime = Prime();
+	//Create our Class with a block size of 3000 bits
+	Prime prime(3000);
 	//Lets time it.
 	//auto start = std::chrono::system_clock::now();
 	
 	// see how many primes under 10 billion
 	size_t maxValueToSearch = 3000; // 10000000000;
-	//if you have no data, lets find some primes 
+	//if you have no data, lets find some primes.  Do 40 blocks
 	prime.FindPrimes(40);
 	// Lets see how mnay we found
 	size_t count = prime.CountPrimes(0,maxValueToSearch);
@@ -50,8 +50,8 @@ int main(int argc, char*argv[])
 	//Lets Save our primes
 	prime.SaveToFile("myPrimes");
 
-	//Lets create a new Prime class instance and load it up with the data we just saved
-	Prime prime2 = Prime();
+	//Lets create a new Prime class instance and load it up with the data we just saved. Data block sizes must be the same.
+	Prime prime2 = Prime(3000);
 	prime2.LoadFromFile("myPrimes");
 
 	size_t count2 =prime2.CountPrimes(0, 3000);
@@ -73,28 +73,28 @@ int main(int argc, char*argv[])
 	std::cout << "0-10000000000000 : " << prime2.CountPrimes(0, 10000000000000) << std::endl;
 
 //	std::bitset<Prime::BitBlockSize> primeAsBitset = 
-//	std::bitset<max> *bStart = primeSieve();
+//	boost::dynamic_bitset<> *bStart = primeSieve();
 //vectorOfBitSets.push_back(bStart);
-//std::bitset<max> *bSecond = primeSieve2(vectorOfBitSets);
+//boost::dynamic_bitset<> *bSecond = primeSieve2(vectorOfBitSets);
 //vectorOfBitSets.push_back(bSecond);
-//std::bitset<max> *bThird = primeSieve2(vectorOfBitSets);
+//boost::dynamic_bitset<> *bThird = primeSieve2(vectorOfBitSets);
 //vectorOfBitSets.push_back(bThird);
-//std::bitset<max> *bForth = primeSieve2(vectorOfBitSets);
+//boost::dynamic_bitset<> *bForth = primeSieve2(vectorOfBitSets);
 //size_t count1 = countPrimes(bStart);
 //size_t count2 = countPrimes(bSecond);
 //size_t count3 = countPrimes(bThird);
 //size_t count4 = countPrimes(bForth);
 //size_t sum = count1 + count2 + count3 + count4;
 //std::cout << "Prime count : " << sum << "\n";
-//std::bitset<max5> *bComp = new std::bitset<max5>();
+//boost::dynamic_bitset<> *bComp = new boost::dynamic_bitset<>();
 //compressBitSet(bStart, bComp);
 //writeBitSetToStream("d:\\binary.bin", bComp);
-////std::bitset<max>* bNext=primeSieve2(bStart);
-//std::bitset<max5> *bCompIn = new std::bitset<max5>();
+////boost::dynamic_bitset<>* bNext=primeSieve2(bStart);
+//boost::dynamic_bitset<> *bCompIn = new boost::dynamic_bitset<>();
 //readBitSetFromStream("d:\\binary.bin", bCompIn);
-//std::bitset<max> *bFromFile = new std::bitset<max>();
+//boost::dynamic_bitset<> *bFromFile = new boost::dynamic_bitset<>();
 //uncompressBitSet(bCompIn, bFromFile);
-////std::bitset<max> *bNext2 = new std::bitset<max>();
+////boost::dynamic_bitset<> *bNext2 = new boost::dynamic_bitset<>();
 ////size_t count2 = countPrimes(bFromFile);
 ////std::cout << "Prime count : " << count2 << "\n";
 //
