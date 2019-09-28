00566ac6056c77952b6ca18cbaca6f061f4c1db6
burhop <mark.burhop@siemens.com>
2019-09-28 19:25:00 -0400

    Test cases for saving and loading of prime files.

--- test/Tests.cpp
+++ test/Tests.cpp
@@ -17,11 +17,16 @@ size_t TestFindPrimes1(int blocks, size_t blocksize, size_t mmaxValueToSearch)
 
 size_t TestFindPrimes2(int blocks, size_t blocksize, size_t mmaxValueToSearch)
 {
+
 	Prime prime(blocksize);
+	prime.DeleteExistingPrimeFiles("test");
 	//if you have no data, lets find some primes
 	prime.FindPrimes(blocks);
 	// Lets see how mnay we found
-	size_t count = prime.CountPrimes(0, mmaxValueToSearch);
+	prime.SaveToFile("test");
+	Prime newPrime(blocksize);
+	newPrime.LoadFromFile("test");
+	size_t count = newPrime.CountPrimes(0, mmaxValueToSearch);
 	return count;
 }
 int Factorial(int number) {
@@ -49,7 +54,7 @@ TEST_CASE("Find Primes using different number of blocks(pass)", "[single-file]")
 	REQUIRE(TestFindPrimes1(10, 3000, 30000) == 3245);
 	REQUIRE(TestFindPrimes1(100, 300, 30000) == 3245);
 	REQUIRE(TestFindPrimes1(1000, 300, 30000) == 3245);
-	REQUIRE(TestFindPrimes1(1000, 300, 300000) == 25998);
+	REQUIRE(TestFindPrimes1(1000, 300, 300000) == 25997);
 
 	//check with zero data sets. No exception since we can give a valid answer
 	REQUIRE(TestFindPrimes1(0, 300, 100) == 0);
@@ -58,6 +63,24 @@ TEST_CASE("Find Primes using different block sizes (fail)", "[single-file]") {
 	REQUIRE_THROWS(TestFindPrimes1(1, 301, 100));
 	REQUIRE_THROWS(TestFindPrimes1(-1, 300, 100));
 	REQUIRE_THROWS(TestFindPrimes1(0xFFFFFFF1, 300, 100));
+}
+
+TEST_CASE("Verify saving and loading of prime files works)", "[single-file]") {
+	REQUIRE(TestFindPrimes2(2, 300, 100) == 25);
+	REQUIRE(TestFindPrimes2(3, 300, 10) == 4);
+	REQUIRE(TestFindPrimes2(3, 300, 900) == 154);
+	REQUIRE(TestFindPrimes2(2, 3000, 3000) == 430);
+	REQUIRE(TestFindPrimes2(2, 3000, 6000) == 783);
+	REQUIRE(TestFindPrimes2(10, 3000, 9000) == 1117);
+	REQUIRE(TestFindPrimes2(10, 3000, 30000) == 3245);
+	REQUIRE(TestFindPrimes2(10, 3000, 30000) == 3245);
+	REQUIRE(TestFindPrimes2(10, 3000, 30000) == 3245);
+	REQUIRE(TestFindPrimes2(100, 300, 30000) == 3245);
+	REQUIRE(TestFindPrimes2(1000, 300, 30000) == 3245);
+	REQUIRE(TestFindPrimes2(1000, 300, 300000) == 25997);
+
+
+
 }
 
 //TEST_CASE("Factorials of 1 and higher are computed (pass)", "[single-file]") {
