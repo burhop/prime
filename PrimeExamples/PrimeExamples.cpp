70a000017c339a197eaef4a47ce0df713629a3c1
burhop <mark.burhop@gmail.com>
2020-01-11 15:21:05 -0500

    Updated code to work with larger array size.

--- PrimeExamples/PrimeExamples.cpp
+++ PrimeExamples/PrimeExamples.cpp
@@ -1,5 +1,32 @@
 // PrimeExamples.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+// Output should look like this depending on how many primes you have searched for.
+
+/* 
+
+Selection Option:
+1 - find and count primes
+2 - read existing files and count primes.
+2
+total integers : 8408427012
+Largest Prime : 210453396701
+Primes Found : 8408427012
+0 - 100 : 25
+101 - 200 : 20
+201 - 300 : 16
+301 - 400 : 16
+0 - 10000 : 1229
+0 - 100000 : 9592
+0 - 1000000 : 78498
+0 - 10000000 : 664579
+0 - 100000000 : 5761455
+0 - 1000000000 : 50847534
+0 - 10000000000 : 455052511
+0 - 100000000000 : 4118054813
+0 - 1000000000000 : 0
+0 - 10000000000000 : 0
+
+*/
 
 //#include "pch.h"
 #include <string>
@@ -9,17 +36,14 @@
 #include <iostream>
 #include <vector>
 #include <bitset>
+const size_t arraySize = 0xFFFFFFF0;
 int main(int argc, char*argv[])
 {
 	//Create our Instance
-	//Prime prime(0xFFFFFFF0);
-	Prime prime(1200000);
-	//Lets time it.
-	//auto start = std::chrono::system_clock::now();
-	//if you have no data, lets find some primes.  Do 4 blocks
+	Prime prime(arraySize);
 	std::cout << " Selection Option: \n";
-	std::cout << "     1 - count primes \n";
-	std::cout << "     2 - read existing files \n";
+	std::cout << "     1 - find and count primes \n";
+	std::cout << "     2 - read existing files and count primes. \n";
 
 	int opt;
 	std::cin >> opt;
@@ -61,7 +85,7 @@ int main(int argc, char*argv[])
 		prime.SaveToFile("myPrimes");
 
 		//Lets create a new Prime class instance and load it up with the data we just saved. Data block sizes must be the same.
-		Prime prime2 = Prime(1200000);
+		Prime prime2 = Prime(arraySize);
 
 		try {
 			prime2.LoadFromFile("myPrimes");
@@ -100,7 +124,19 @@ int main(int argc, char*argv[])
 		std::cout << "total integers: " << prime.GetMaxCount() << std::endl;
 		std::cout << "Largest Prime : " << prime.GetMaxPrime() << std::endl;
 		std::cout << "Primes Found  : " << prime.GetMaxCount() << std::endl;
-
+		std::cout << "0-100   : " << prime.CountPrimes(0, 100) << std::endl;
+		std::cout << "101-200 : " << prime.CountPrimes(101, 200) << std::endl;
+		std::cout << "201-300 : " << prime.CountPrimes(201, 300) << std::endl;
+		std::cout << "301-400 : " << prime.CountPrimes(301, 400) << std::endl;
+		std::cout << "0-10000 : " << prime.CountPrimes(0, 10000) << std::endl;
+		std::cout << "0-100000 : " << prime.CountPrimes(0, 100000) << std::endl;
+		std::cout << "0-1000000 : " << prime.CountPrimes(0, 1000000) << std::endl;
+		std::cout << "0-10000000 : " << prime.CountPrimes(0, 10000000) << std::endl;
+		std::cout << "0-100000000 : " << prime.CountPrimes(0, 100000000) << std::endl;
+		std::cout << "0-1000000000 : " << prime.CountPrimes(0, 1000000000) << std::endl;
+		std::cout << "0-10000000000 : " << prime.CountPrimes(0, 10000000000) << std::endl;
+		std::cout << "0-100000000000 : " << prime.CountPrimes(0, 100000000000) << std::endl;
+		std::cout << "0-1000000000000 : " << prime.CountPrimes(0, 1000000000000) << std::endl;
 		std::cout << "0-10000000000000 : " << prime.CountPrimes(0, 10000000000000) << std::endl;
 	}
 return 0;
